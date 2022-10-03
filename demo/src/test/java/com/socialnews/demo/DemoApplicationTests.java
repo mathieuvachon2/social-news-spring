8deb094af8a6893a40e8a84b96bbe3487835edd3
mathieuvachon2 <vmat96@gmail.com>
2022-10-03 19:37:08 -0400

    adding structure of project

---
+++ demo/src/test/java/com/socialnews/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnews.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
